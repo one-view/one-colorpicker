987eecd24741b6d69c8b5f75929dae20dd4dfa3f
roei <roeichow@gmail.com>
2018-04-12 13:52:15 +0800

    update: remove build js.map

--- webpack.config.js
+++ webpack.config.js
@@ -96,8 +96,8 @@ module.exports = {
   devtool: '#eval-source-map'
 }
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+if (process.env.NODE_ENV !== 'development') {
+  module.exports.devtool = false
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
